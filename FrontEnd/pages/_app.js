5f2795e507fe165c4573d69ec9340c02f5029a3a
Tim Roty <timroty@users.noreply.github.com>
2023-04-17 17:56:06 -0500

    Add Google Anayltics

--- FrontEnd/pages/_app.js
+++ FrontEnd/pages/_app.js
@@ -1,6 +1,8 @@
 import { Auth } from '@supabase/ui'
 import { supabase } from '../lib/initSupabase'
-import Head from "next/head";
+import Head from 'next/head';
+import Script from 'next/script'
+
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -8,6 +10,21 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="shortcut icon" href="/Podcapsule.png" />
       </Head>
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive" 
+        dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
       <main className={'dark'}>
       <Auth.UserContextProvider supabaseClient={supabase}>
         <Component {...pageProps} />
