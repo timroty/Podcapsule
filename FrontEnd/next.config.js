1127d0511b3250fdcd314c49508dc184a8c5f3ee
Tim Roty <timroty@users.noreply.github.com>
2023-09-07 18:11:41 -0500

    Update Next.js config

--- FrontEnd/next.config.js
+++ FrontEnd/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[]",
+  },
 }
 
 module.exports = nextConfig
